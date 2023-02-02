ccb0d67db8892ddce4739a1642f79d4cc42088d9
cchan6969 <92964126+cchan6969@users.noreply.github.com>
2023-02-02 13:05:04 +0800

    changes

---
+++ test compile/Untitled-1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>  //defines some variables for IO
+#include <string>   // for string input
+#include <iostream> // cin & cout
+using namespace std;
+int main() 
+{
+    string input;
+    printf("Hello World\n");
+    cin >> input;
+    return 0;
+}
